32030b2f70c422fde537a86cde57573ae2e04f51
Christian <christian.blades@careerbuilder.com>
2015-06-23 23:20:16 -0400

    fixup!

--- main.go
+++ main.go
@@ -13,23 +13,22 @@ import (
 func main() {
 	var opts struct {
 		StatsdServer string `short:"s" long:"host" env:"STATSD_HOST" default:"localhost:8125" required:"true" description:"statsd hostname/ip and port"`
-		Delay        int64  `short:"d" long:"delay" required:"true" default:"50" description:"delay (in milliseconds) between stats emissions"`
+		Delay        int64  `short:"d" long:"delay" env:"DELAY" required:"true" default:"50" description:"delay (in milliseconds) between stats emissions"`
 	}
 	_, err := flags.Parse(&opts)
 	if err != nil {
 		log.Fatal("unable to parse command line arguments")
 	}
 
-	conn, err := net.Dial("udp", opts.StatsdServer)
-	if err != nil {
-		log.Fatal(err)
-	}
-
 	log.Println("Emitting")
 	ticker := time.NewTicker(time.Millisecond * time.Duration(opts.Delay))
 	x := 0.0
 	for _ = range ticker.C {
-		fmt.Fprintf(conn, "sinewave:%f|c\n", x)
-		x = math.Mod(x, 360)
+		conn, _ := net.Dial("udp", opts.StatsdServer)
+		fmt.Fprintf(conn, "sinewave:%d|c\n", int64(math.Sin(x)*100.0)+100)
+		conn.Close()
+
+		x = math.Mod(x+1, 360)
+		fmt.Print("*")
 	}
 }
